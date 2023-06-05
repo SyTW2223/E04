3a7b1091c73d8be6eba3753f6d9ad00dc5215de5
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2023-06-05 12:41:59 +0100

    añadir propiedad frutas

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,13 +4,14 @@ const UserSchema = new mongoose.Schema({
     user: {
         type: String,
         required: true,
-        trim: true,
     },
     password: {
         type: String,
         required: true,
-        trim: true,
     },
+    fruits: [{
+        type: Number
+    }]
 });
 
 export const User = mongoose.model("User", UserSchema);
