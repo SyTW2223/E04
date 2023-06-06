68a2183b354ebe33c3d5100bef1edb5aa6a177f8
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2023-06-06 17:00:37 +0100

    añadir ruta addfavfruit

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyJwt } from "../middlewares/verifyJwt.js";
-import { home, profile } from "../controllers/user.controller.js";
+import { home, profile, addFavFruit } from "../controllers/user.controller.js";
 
 export const userRouter = express.Router();
 
 userRouter.get("/home", home);
 userRouter.post("/profile", [verifyJwt], profile);
+userRouter.post("/addfavfruit", [verifyJwt], addFavFruit);
