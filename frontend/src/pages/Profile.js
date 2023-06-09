f4713080f9bbc732b4a77932c2a5e2637a313152
Adahi Oval <alu0101242071@ull.edu.es>
2023-06-09 12:28:29 +0100

    Arregladas las paginas y reorganizacion de elementos

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div className='base'>Profile</div>
+    <div className='App'>
+      <h1 className='page-header'>Perfil</h1>
+    </div>
   )
 }
 
