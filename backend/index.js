a7322022772024e7c9d7632f28c34bb64efcfa2a
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2023-06-09 23:00:09 +0100

    usar cors

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import { userRouter } from "./routes/user.route.js";
 import "./config/db.js"
 
 export const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
