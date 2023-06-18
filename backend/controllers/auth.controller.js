913d91f39e8a554bffb36cfa6989f10551c2bb25
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2023-06-18 16:34:29 +0100

    cambiar controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,41 +3,41 @@ import bcrypt from "bcryptjs";
 import { User } from "../models/user.model.js";
 
 export const signUp = async (req, res) => {
-  try {
-    const user = new User({
-      user: req.body.user,
-      password: bcrypt.hashSync(req.body.password, 8),
-      fruits: req.body.fruits
-    });
+    try {
+        const user = new User({
+            user: req.body.user,
+            password: bcrypt.hashSync(req.body.password, 8),
+            fruits: req.body.fruits
+        });
 
-    await user.save();
-    res.status(200).json({ message: "User was registered successfully", user: user.user });
-  } catch (error) {
-    res.status(500).json({ message: "Error occurred while registering user" });
+        await user.save();
+        res.status(200).json({ message: "User was registered successfully", user: user.user });
+    } catch (error) {
+        res.status(500).json({ message: "Error occurred while registering user" });
   }
 };
 
 export const signIn = async (req, res) => {
-  try {
-    const user = await User.findOne({ user: req.body.user });
+    try {
+        const user = await User.findOne({ user: req.body.user });
 
-    if (!user) {
-      return res.status(404).send({ message: "User not found" });
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+        const validPassword = bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
+        if (!validPassword) {
+            return res.status(401).send({ message: "Invalid password" });
+        }
+        const token = jsonwebtoken.sign(
+            { user: user.user },
+            "secret",
+            { expiresIn: "86400s" }
+        );
+        res.send({ token: token });
+    } catch (error) {
+        res.status(500).send({ message: error });
     }
-    const validPassword = bcrypt.compareSync(
-      req.body.password,
-      user.password
-    );
-    if (!validPassword) {
-      return res.status(401).send({ message: "Invalid password" });
-    }
-    const token = jsonwebtoken.sign(
-      { user: user.user },
-      "secret",
-      { expiresIn: "86400s" }
-    );
-    res.send({ token: token });
-  } catch (error) {
-    res.status(500).send({ message: error });
-  }
 };
